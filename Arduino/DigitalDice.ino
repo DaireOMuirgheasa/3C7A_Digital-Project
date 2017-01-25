cc87d6555eeb35f8fbfdfd1ed35bb88678de6bfc
SirWhy <andersm3@tcd.ie>
2017-01-25 14:41:53 +0000

    Create DigitalDice.ino
    Just added some bare bones skeleton code

---
+++ Arduino/DigitalDice.ino
@@ -0,0 +1,25 @@
+//
+//  Digital Dice Testbed
+//  Created 25-1-17
+//  Group 7
+//    Mark Anderson
+//    Eoin Gogarty
+//    Rob Hanson
+//    Daire Morrissy
+//
+
+#define PASS 13 // PASS/FAIL LEDs
+#define FAIL 12
+
+void setup()
+{
+  pinMode(PASS, output);
+  pinMode(FAIL, output);
+  
+  // other stuff
+}
+
+void loop()
+{
+  // Don't actually need code in here as we only want to test once.
+}
