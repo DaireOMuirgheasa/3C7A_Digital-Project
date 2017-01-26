152ed0eea5b4fbc8760abb22bc6b7b25faf12374
SirWhy <andersm3@tcd.ie>
2017-01-26 20:53:21 +0000

    Update DigitalDice.ino
    Added in the logic test with bit manipulation, tested with hardware

--- Arduino/DigitalDice.ino
+++ Arduino/DigitalDice.ino
@@ -57,12 +57,47 @@ void setup() {
 #define PASS 13 // PASS/FAIL LEDs
 #define FAIL 12
 
+//  Truth Table
+//     A B C D | Decimal
+// 0 | 1 1 0 1 | 13 
+// 1 | 0 0 1 0 | 2
+// 2 | 1 0 0 0 | 8
+// 3 | 1 0 1 0 | 10
+// 4 | 1 1 0 0 | 12
+// 5 | 1 1 1 0 | 14
+
+char logic_out[6]  = {13, 2, 8, 10, 12, 14};
+char reading;
+boolean failed;
+
+void logicTest()
+{
+  // Logic Test
+  DDRD = DDRD | B11110000; // Using Bit Manipulation here as we are dealing with a parallel bus
+  DDRB = DDRB | B00001111; // Using a bitwise OR means we don't accidentally change something
+  for (int i = 0; i < 6; i++){ // run through binary sequence on pins 
+    PORTD = (i << 4); // output binary number on pins 4,5,6
+    delay(10);
+    reading = PINB & B00001111; // check inputs on B port (A - Pin 11, B - 10, C - 9, D - 8)
+    if (reading != logic_out[i]){ 
+      failed = true;
+      //break;
+    }
+  }
+  
+  PORTD = 0;
+  reading = PIND & B10000000;
+  if (reading != 128 || failed)
+    digitalWrite(FAIL, HIGH);
+  else
+    digitalWrite(PASS, HIGH);  
+}
 void setup()
 {
   pinMode(PASS, OUTPUT);
   pinMode(FAIL, OUTPUT);
-  
-  // other stuff
+
+  logicTest();
 }
 
 void loop()
